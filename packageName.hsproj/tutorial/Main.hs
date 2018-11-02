1d6f8274e2af47855f8514741e79cf3ab20cdce2
baby7305 <songhaibei7305@163.com>
2018-11-02 16:30:00 +0800

    first steps

---
+++ packageName.hsproj/tutorial/Main.hs
@@ -0,0 +1,14 @@
+inc::Int -> Int--type signature
+inc x = x + 1--function equation
+
+double::Int -> Int
+double x = 2 * x
+
+exclaim::String -> String
+exclaim sentence = sentence++"!"
+
+average::Float -> Float -> Float
+average a b = (a + b)/2.0
+
+square::Int -> Int
+square x = x * x
